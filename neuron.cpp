5daac174991a0aa5ad33f1af2a72f59ca4e0b990
breymond <32453756+breymond@users.noreply.github.com>
2017-11-01 11:48:42 +0100

    add

--- neuron.cpp
+++ neuron.cpp
@@ -4,17 +4,14 @@
 #include "neuron.hpp"
 
 
-
+Neuron::Neuron() : clock(0), spike_buffer(delay_steps+1)
+{};
 
 double Neuron::getv_m()
 {
 	return v_m;
 }
 
-double Neuron::geti_ext()
-{
-	return i_ext;
-}
 
 double Neuron::gett_spike()
 {
@@ -28,17 +25,6 @@ long Neuron::getn_spikes()
 }
 
 
-void Neuron::setc1andc2(double h_, double tau_m, double R_)
-{
-	c1 = exp(-h_/tau_m);
-	c2 = R_*(1.0-c1);	
-}
-
-void Neuron::seti_ext(double x)
-{
-	i_ext = x;	
-}
-
 void Neuron::receive_spike(unsigned long arrival, double j)
 {
 	const size_t t_out = arrival%(delay_steps + 1);
@@ -48,28 +34,33 @@ void Neuron::receive_spike(unsigned long arrival, double j)
 
 bool Neuron::update(unsigned long steps)
 {
-	if(steps == 0) return false;
+	if(steps == 0)return false;
 	
 	bool spike = false;
 	double t_stop = clock + steps;
-	
 	while (clock < t_stop)
 	{
 		int t_in = clock % (delay_steps +1);
+		//cout <<"v_m = " << v_m <<"\t" << "v_th_ = " << v_th_ <<"\n";
+		
+		
+		
+		
 		if(v_m > v_th_)
 		{
 			++n_spikes;
 			t_spike = clock;
 			spike = true;
 		}
-		
+		else
+		{
+			cout <<"v_m = " <<v_m <<"\n";
+			v_m += exp(-h_/tau_m)*v_m + R_*(1.0-exp(-h_/tau_m)) *i_ext + spike_buffer[t_in];
+			//cout <<"nouveau v_m = " << v_m <<"\n";
+		}
 		if((t_spike > 0) and (clock - t_spike < refractory_steps))
 		{
 			v_m = 0.0;
-		}
-		else
-		{
-			v_m = c1*v_m + c2 *i_ext + spike_buffer[t_in];
 		}
 		spike_buffer[t_in]=0.0;
 		++clock;
